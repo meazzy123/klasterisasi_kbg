620e0f9ab4396231907492da5718cd0b2213da43
meazzy123 <105058075+meazzy123@users.noreply.github.com>
2024-01-02 20:33:14 +0700

    Update klasterisasi.py

--- klasterisasi.py
+++ klasterisasi.py
@@ -8,6 +8,7 @@ from sklearn.metrics import davies_bouldin_score
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
 from matplotlib.animation import FuncAnimation
+import plotly.graph_objects as go
 
 
 def intro():
@@ -107,7 +108,6 @@ def tahun2022():
         data22["Cluster"] = y_cluster22
         colors = [plt.cm.nipy_spectral(float(i) / n_clust) for i in range(n_clust)]
 
-        # Membuat dataframe baru dengan informasi yang diperlukan
         df_plotly = pd.DataFrame(
             {
                 "Total KBG Perempuan": data22["Total KBG Perempuan"],
@@ -142,6 +142,46 @@ def tahun2022():
         # Menampilkan plot
         st.plotly_chart(fig)
         st.write(data22)
+
+        cluster_totals = (
+            df_plotly.groupby("Cluster")["Total KBG Perempuan"].sum().reset_index()
+        )
+        total_provinsi = (
+            df_plotly.groupby("Cluster")["Provinsi"]
+            .count()
+            .reset_index(name="Total Provinsi")
+        )
+
+        # Bar plot untuk menunjukkan berapa banyak data di masing-masing klaster (vertical)
+        fig_bar = px.bar(
+            df_plotly,
+            x="Cluster",
+            y="Total KBG Perempuan",  # Menggunakan 'y' sebagai sumbu vertikal
+            orientation="v",  # Mengatur orientasi ke vertical
+            title="Jumlah Data di Setiap Klaster",
+            labels={"Cluster": "Klaster", "Total KBG Perempuan": "Jumlah Data"},
+            hover_data=["Provinsi"],
+            color="Provinsi",  # Memberikan warna berbeda pada setiap klaster
+            # color_discrete_sequence=colors,  # Menggunakan palet warna bawaan Plotly
+        )
+
+        # Menambahkan total jumlah data pada masing-masing klaster
+        for i, total, total_prov in zip(
+            cluster_totals["Cluster"],
+            cluster_totals["Total KBG Perempuan"],
+            total_provinsi["Total Provinsi"],
+        ):
+            fig_bar.add_annotation(
+                x=i,
+                y=total,
+                text=f"Total KBG Perempuan: {total}<br>Total Provinsi: {total_prov}",
+                showarrow=True,
+                arrowhead=1,
+                ax=0,
+                ay=-30,
+            )
+
+        st.plotly_chart(fig_bar)
 
     k_means(clust)
 
@@ -200,7 +240,6 @@ def tahun2021():
         data21["Cluster"] = y_cluster21
         colors = [plt.cm.nipy_spectral(float(i) / n_clust) for i in range(n_clust)]
 
-        # Membuat dataframe baru dengan informasi yang diperlukan
         df_plotly = pd.DataFrame(
             {
                 "Total KBG Perempuan": data21["Total KBG Perempuan"],
@@ -236,15 +275,258 @@ def tahun2021():
         st.plotly_chart(fig)
         st.write(data21)
 
+        cluster_totals = (
+            df_plotly.groupby("Cluster")["Total KBG Perempuan"].sum().reset_index()
+        )
+        total_provinsi = (
+            df_plotly.groupby("Cluster")["Provinsi"]
+            .count()
+            .reset_index(name="Total Provinsi")
+        )
+
+        # Bar plot untuk menunjukkan berapa banyak data di masing-masing klaster (vertical)
+        fig_bar = px.bar(
+            df_plotly,
+            x="Cluster",
+            y="Total KBG Perempuan",  # Menggunakan 'y' sebagai sumbu vertikal
+            orientation="v",  # Mengatur orientasi ke vertical
+            title="Jumlah Data di Setiap Klaster",
+            labels={"Cluster": "Klaster", "Total KBG Perempuan": "Jumlah Data"},
+            hover_data=["Provinsi"],
+            color="Provinsi",  # Memberikan warna berbeda pada setiap klaster
+            # color_discrete_sequence=colors,  # Menggunakan palet warna bawaan Plotly
+        )
+
+        # Menambahkan total jumlah data pada masing-masing klaster
+        for i, total, total_prov in zip(
+            cluster_totals["Cluster"],
+            cluster_totals["Total KBG Perempuan"],
+            total_provinsi["Total Provinsi"],
+        ):
+            fig_bar.add_annotation(
+                x=i,
+                y=total,
+                text=f"Total KBG Perempuan: {total}<br>Total Provinsi: {total_prov}",
+                showarrow=True,
+                arrowhead=1,
+                ax=0,
+                ay=-30,
+            )
+
+        st.plotly_chart(fig_bar)
+        # # Bar plot untuk menunjukkan berapa banyak data di masing-masing klaster (vertical)
+        # fig_bar = px.bar(
+        #     df_plotly,
+        #     x="Cluster",
+        #     y="Total KBG Perempuan",  # Menggunakan 'y' sebagai sumbu vertikal
+        #     orientation="v",  # Mengatur orientasi ke vertical
+        #     title="Jumlah Data di Setiap Klaster",
+        #     labels={"Cluster": "Klaster", "Total KBG Perempuan": "Jumlah Data"},
+        #     hover_data=["Provinsi"],
+        #     color="Provinsi",  # Memberikan warna berbeda pada setiap klaster
+        #     # color_discrete_sequence=colors,  # Menggunakan palet warna bawaan Plotly
+        # )
+
+        # st.plotly_chart(fig_bar)
+
     k_means(clust)
+
+
+def train_and_predict(year, x_train, input_data, clust):
+    # Scaling data training
+    scaler = MinMaxScaler()
+    x_train_scaled = scaler.fit_transform(x_train)
+
+    # Melatih model KMeans
+    kmeans = KMeans(n_clusters=clust)
+    kmeans.fit(x_train_scaled)
+
+    # Scaling data input using the same scaler
+    input_data_scaled = scaler.transform(np.array([sum(input_data)]).reshape(1, -1))
+
+    # Lakukan prediksi klaster
+    predicted_cluster = kmeans.predict(input_data_scaled)
+
+    # Dapatkan label klaster untuk setiap data di dataset
+    cluster_labels = kmeans.labels_
+
+    return predicted_cluster[0], cluster_labels
+
+
+def show_scatter_plot(dataset, predicted_cluster, input_data, nama_provinsi, clust):
+    # Membuat dataframe baru dengan informasi yang diperlukan
+    df_plotly = pd.DataFrame(
+        {
+            "Total KBG Perempuan": dataset["Total KBG Perempuan"],
+            "Provinsi": dataset["Provinsi"],
+            "Cluster": dataset["Cluster"],
+            "Komnas Perempuan": dataset["Komnas Perempuan"],
+            "Data Lembaga Mitra": dataset["Data Lembaga Mitra"],
+            "Badilag": dataset["Badilag"],
+        }
+    )
+
+    # Membuat scatter plot interaktif dengan Plotly
+    fig = px.scatter(
+        df_plotly,
+        x="Total KBG Perempuan",
+        y="Provinsi",
+        color="Cluster",
+        color_discrete_sequence=px.colors.qualitative.Set1,
+        labels={"Cluster": "Cluster"},
+        title="Persebaran Kejahatan Berbasis Gender Terhadap Perempuan",
+        hover_name="Provinsi",
+        hover_data={
+            "Komnas Perempuan": True,
+            "Data Lembaga Mitra": True,
+            "Badilag": True,
+            "Total KBG Perempuan": True,
+            "Provinsi": False,
+            "Cluster": True,
+        },
+    )
+
+    # Menambahkan titik prediksi ke dalam plot
+    fig.add_trace(
+        go.Scatter(
+            x=[input_data[0]],
+            y=[nama_provinsi],
+            mode="markers",
+            marker=dict(color="red"),
+            name="Prediksi",
+            hoverinfo="text",  # Mengganti "name+x+y" menjadi "text"
+            # text=f"Total KBG Perempuan: {total}<br>Total Provinsi: {total_prov}",
+            text=[
+                f"<b>{nama_provinsi}</b><br><br>Total KBG Perempuan: {sum(input_data)}<br>Komnas Perempuan: {input_data[0]}<br>Data Lembaga Mitra: {input_data[1]}<br>Badilag: {input_data[2]}<br>Cluster: {predicted_cluster}"
+            ],
+            showlegend=True,
+        )
+    )
+
+    # Menampilkan plot
+    st.plotly_chart(fig)
+    # Tambahkan data hasil prediksi ke dalam DataFrame df_plotly
+    df_prediksi = pd.DataFrame(
+        {
+            "Cluster": [predicted_cluster],
+            "Total KBG Perempuan": [sum(input_data)],
+            "Provinsi": ["Prediksi"],
+        }
+    )
+    df_plotly = pd.concat([df_plotly, df_prediksi], ignore_index=True)
+
+    # Bar plot untuk menunjukkan berapa banyak data di masing-masing klaster (vertical)
+    fig_bar = px.bar(
+        df_plotly,
+        x="Cluster",
+        y="Total KBG Perempuan",
+        orientation="v",
+        title="Jumlah Data di Setiap Klaster",
+        labels={"Cluster": "Klaster", "Total KBG Perempuan": "Jumlah Data"},
+        hover_data=["Provinsi"],  # Menambahkan Provinsi pada hover
+        color="Provinsi",  # Warna merah untuk data prediksi
+        color_discrete_map={"Prediksi": "red"},
+    )
+
+    # Menampilkan bar plot vertical
+    st.plotly_chart(fig_bar)
+
+
+def barplot_prediksi(n_clust, df_plotly, input_data, predicted_cluster):
+    # Tambahkan data hasil prediksi ke dalam DataFrame df_plotly
+    df_prediksi = pd.DataFrame(
+        {
+            "Cluster": [predicted_cluster],
+            "Total KBG Perempuan": [sum(input_data)],
+            "Provinsi": ["Prediksi"],
+        }
+    )
+    df_plotly = pd.concat([df_plotly, df_prediksi], ignore_index=True)
+
+    # Bar plot untuk menunjukkan berapa banyak data di masing-masing klaster (vertical)
+    fig_bar = px.bar(
+        df_plotly,
+        x="Cluster",
+        y="Total KBG Perempuan",
+        orientation="v",
+        title="Jumlah Data di Setiap Klaster",
+        labels={"Cluster": "Klaster", "Total KBG Perempuan": "Jumlah Data"},
+        hover_data=["Provinsi"],  # Menambahkan Provinsi pada hover
+    )
+
+    # Menampilkan bar plot vertical
+    st.plotly_chart(fig_bar)
+
+
+def prediksi():
+    year = st.selectbox("Pilih tahun untuk prediksi", ["2021", "2022"])
+
+    # Membaca dataset
+    dataset = pd.read_excel("dataset_kbg_perempuan.xlsx", sheet_name=year)
+
+    # Menambahkan kolom Total KBG Perempuan
+    dataset["Total KBG Perempuan"] = (
+        dataset["Komnas Perempuan"] + dataset["Data Lembaga Mitra"] + dataset["Badilag"]
+    )
+
+    # Menampilkan dataset
+    st.header("Dataset")
+    st.write(dataset)
+
+    # Mempersiapkan data untuk training
+    x_train = dataset[["Total KBG Perempuan"]].values
+
+    # Masukkan data prediksi
+    nama_provinsi = st.text_input("Masukkan Nama Provinsi:")
+    komnas_perempuan = st.number_input("Jumlah KBG Perempuan dari Komnas Perempuan:", 0)
+    data_lembaga_mitra = st.number_input(
+        "Jumlah KBG Perempuan dari Data Lembaga Mitra:", 0
+    )
+    badilag = st.number_input("Jumlah KBG Perempuan dari Badilag:", 0)
+
+    # Pilihan jumlah kluster
+    clust = st.slider("Pilih jumlah kluster:", 2, 10, 3, 1, key="kluster")
+
+    # Button untuk melakukan prediksi
+    if st.button("Prediksi Klaster"):
+        input_data = [komnas_perempuan, data_lembaga_mitra, badilag]
+        predicted_cluster, cluster_labels = train_and_predict(
+            year, x_train, input_data, clust
+        )
+
+        # Tambahkan kolom 'Cluster' ke dalam DataFrame dataset
+        dataset["Cluster"] = cluster_labels
+
+        st.success(
+            f"Provinsi {nama_provinsi} pada tahun {year} diprediksi masuk ke dalam Klaster {predicted_cluster}"
+        )
+
+        # Menampilkan scatter plot
+        show_scatter_plot(dataset, predicted_cluster, input_data, nama_provinsi, clust)
+        # barplot_prediksi(clust, df_plotly, input_data, predicted_cluster)
+    # st.header("Prediksi Klaster")
+
+    # # Pilih tahun untuk prediksi
+    # tahun_prediksi = st.selectbox("Pilih tahun untuk prediksi", ["2021", "2022"])
+
+    # # Masukkan data prediksi
+    # nama_provinsi = st.text_input("Masukkan Nama Provinsi:")
+    # komnas_perempuan = st.number_input("Jumlah KBG Perempuan dari Komnas Perempuan:", 0)
+    # data_lembaga_mitra = st.number_input(
+    #     "Jumlah KBG Perempuan dari Data Lembaga Mitra:", 0
+    # )
+    # badilag = st.number_input("Jumlah KBG Perempuan dari Badilag:", 0)
+    # clust = st.slider("Pilih jumlah kluster:", 2, 10, 3, 1)
 
 
 pages = {
     "-": intro,
     "2021": tahun2021,
     "2022": tahun2022,
+    "Prediksi": prediksi,
 }
-page = st.sidebar.selectbox("Pilih tahun", pages.keys())
+st.sidebar.header("Menu")
+page = st.sidebar.selectbox("Pilih Menu", pages.keys())
 pages[page]()
 
 
